d8216240dbc8b74036591d6895f1d744b937fe51
Igor Simovic <igor.simovic@cubesoft.rs>
2017-12-10 13:57:51 +0100

    Migrations configuration and initial migrations

---
+++ NtcCollabWebAPI/Entities/Core/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NtcCollabWebAPI.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedUtc { get; set; }
+        public DateTime ModifiedUtc { get; set; }
+    }
+}
